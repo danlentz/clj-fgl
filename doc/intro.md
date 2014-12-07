010f2ff8a47fbca923c0f9dd4a00ce0eb54159ed
Dan Lentz <danlentz@gmail.com>
2014-12-06 20:19:23 -0500

    Initial

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clj-fgl
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/clj_fgl/core_test.clj
@@ -0,0 +1,7 @@
+(ns clj-fgl.core-test
+  (:require [clojure.test :refer :all]
+            [clj-fgl.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
