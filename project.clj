94bdd33a998db80c8d10fca5b92b9e7b262b5bf6
Dan Lentz <danlentz@gmail.com>
2014-12-06 21:17:37 -0500

    updated: initial description and dependencies

--- project.clj
+++ project.clj
@@ -1,6 +1,10 @@
 (defproject clj-fgl "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Functional Graph Library"
+  :url "http://github.com/danlentz/clj-fgl"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [incanter            "1.5.5"]
+                 [clj-tuple           "0.1.7"]
+                 [print-foo           "1.0.1"]
+                 ])
