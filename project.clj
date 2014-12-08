154f974ddcf086fe2a923234ae52e4459eb17a1a
Dan Lentz <danlentz@gmail.com>
2014-12-08 08:50:20 -0500

    updated: group/project; require clj-uuid

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
-(defproject clj-fgl "0.1.0-SNAPSHOT"
+(defproject danlentz/clj-fgl "0.1.0-SNAPSHOT"
   :description "Functional Graph Library"
   :url "http://github.com/danlentz/clj-fgl"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
+                 [danlentz/clj-uuid   "0.0.6-SNAPSHOT"]
                  [incanter            "1.5.5"]
                  [clj-tuple           "0.1.7"]
                  [print-foo           "1.0.1"]
