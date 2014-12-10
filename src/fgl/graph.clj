f99dfaf046c2c068ac672e5c76d8f87f38255545
Dan Lentz <danlentz@gmail.com>
2014-12-10 08:12:54 -0500

    updated: refactor terminology

--- src/fgl/graph.clj
+++ src/fgl/graph.clj
@@ -1,4 +1,4 @@
-(ns fgl.triples
+(ns fgl.graph
   (:refer-clojure :exclude [])
   (:require [clojure.pprint :as pp])
   (:require [clojure.core.reducers :as r])
@@ -63,39 +63,39 @@
 ;; Graph / GraphContainer Protocol
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defrecord Graph [id indices triples])
+(defrecord Graph [id index edges])
 
 (defprotocol GraphContainer
   (id       [this])
-  (indices  [this])
-  (triples  [this]))
+  (index    [this])
+  (edges    [this]))
 
 (extend-type Graph GraphContainer
              (id [this]
                (:id this))
-             (indices [this]
-               (:indices this))
-             (triples [this]
-               (:triples this)))
+             (index [this]
+               (:indexes this))
+             (edges [this]
+               (:edges this)))
 
 
 (defmethod print-method Graph [g ^java.io.Writer w]
   (.write w "#<Graph ")
   (.write w (str (id g)))
   (.write w " (")
-  (.write w (str (count (triples g))))
-  (.write w " triples)>"))
+  (.write w (str (count (edges g))))
+  (.write w " edges)>"))
 
 (extend-type clojure.lang.PersistentHashSet GraphContainer
              (id [this]
                (if-let [g (@db this)]
                  (id g)
                  (uuid/v4)))
-             (indices [this]
+             (index [this]
                (if-let [g (@db this)]
-                 (indices g)
+                 (index g)
                  {}))
-             (triples [this]
+             (edges [this]
                (assert (every? #(= 3 (count %)) this))
                this))
 
@@ -107,18 +107,18 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
-(defn make-index [triples key1 key2 key3]
-  (reduce (fn [i0 triple]
-            (let [i1 (or (i0 (key1 triple)) {})
-                  i2 (or (i1 (key2 triple)) {})]
+(defn make-subindex [edge-collection key1 key2 key3]
+  (reduce (fn [i0 e]
+            (let [i1 (or (i0 (key1 e)) {})
+                  i2 (or (i1 (key2 e)) {})]
               (assoc i0
-                (key1 triple)
+                (key1 e)
                 (assoc i1
-                  (key2 triple)
+                  (key2 e)
                   (assoc i2
-                    (key3 triple)
-                    triple)))))
-    {} triples))
+                    (key3 e)
+                    e)))))
+    {} edge-collection))
 
 
 ;;;;
@@ -128,7 +128,7 @@
 ;;; in performance
 ;;;;
 
-;; (defn make-index [triples key1 key2 key3]
+;; (defn make-index [edges key1 key2 key3]
 ;;   (r/fold util/rmerge (fn
 ;;                         ([] {})
 ;;                         ([i0 triple]
@@ -142,7 +142,7 @@
 ;;                                  (assoc i2
 ;;                                    (key3 triple)
 ;;                                    triple))))))
-;;     triples))
+;;     edges))
 
 ;;;;
 ;;; The following index implementation creates a set for the innermost
@@ -150,7 +150,7 @@
 ;;; currently in use. 
 ;;;;
 
-;; (defn make-index [triples key1 key2 key3]
+;; (defn make-index [edges key1 key2 key3]
 ;;   (reduce (fn [i0 triple]
 ;;             (let [i1 (or (i0 (key1 triple)) {})
 ;;                   i2 (or (i1 (key2 triple)) #{})]
@@ -159,26 +159,26 @@
 ;;                 (assoc i1
 ;;                   (key2 triple)
 ;;                   (conj i2 (key3 triple))))))
-;;     {} triples))
+;;     {} edges))
 
 
-(defn add-index [g key1 key2 key3]
+(defn add-subindex [g key1 key2 key3]
   (->Graph (id g)
-    (assoc (indices g)
-      [key1 key2 key3] (make-index (triples g) key1 key2 key3))
-    (triples g)))
+    (assoc (index g)
+      [key1 key2 key3] (make-subindex (edges g) key1 key2 key3))
+    (edges g)))
 
 
 (defn make-graph
   ([tuples]
      (or (@db (set tuples))
-       (make-graph (node) (triples (set tuples)))))
+       (make-graph (node) (edges (set tuples)))))
   ([id tuples]
      (assert (node? id))
-     (-> (->Graph id {} (triples (set tuples))
-       (add-index s p o)
-       (add-index p o s)
-       (add-index o s p))))
+     (-> (->Graph id {} (edges (set tuples)))
+       (add-subindex s p o)
+       (add-subindex p o s)
+       (add-subindex o s p))))
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -188,7 +188,6 @@
 (defprotocol GraphBuilder
   (graph [this]))
 
-;; TODO: Perhaps specialize on more abstract classes eg., Collection, ...?
 
 (extend-type Graph GraphBuilder
              (graph [this]
@@ -219,28 +218,28 @@
 ;; Incremental Graph (de)Contruction and (de)Indexing
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defn- index-triple [[S P O]]
-  {[o s p] {:+ {[O S] {P (tuple S P O)}}}
-   [p o s] {:+ {[P O] {S (tuple S P O)}}}
-   [s p o] {:+ {[S P] {O (tuple S P O)}}}})
+(defn- index-edge [[S P O]]
+  {[o s p] {:+ {[O S] {P (edge S P O)}}}
+   [p o s] {:+ {[P O] {S (edge S P O)}}}
+   [s p o] {:+ {[S P] {O (edge S P O)}}}})
 
-(defn- add-triple [g [S P O]]
-  (let [tups (conj (triples g) (tuple S P O))]
+(defn- add-edge [g [S P O]]
+  (let [tups (conj (edges g) (tuple S P O))]
     (or (@db tups)
-      (let [k (keys (indices g))
+      (let [k (keys (indexes g))
             p (map (index-triple [S P O]) k)
-            o (map (indices g) k)
+            o (map (indexes g) k)
             n (zipmap k (map diff/patch-unchecked o p))]
         (->Graph (node) n tups)))))
 
-(defn add-triples 
-  "Efficiently add and index 0 or more triples to an existing graph,
+(defn add-edges 
+  "Efficiently add and index 0 or more edges to an existing graph,
   returning a new graph as the result."
   [g & more]
   (reduce add-triple g more))
 
-;; (add-triples +NULL+ [:a :b :c] [:a :b :d] [:a :a :a] [:a :a :b])
-;;  => #<Graph 80dc0041-0476-1196-9bc3-7831c1bbb832 (4 triples)>
+;; (add-edges +NULL+ [:a :b :c] [:a :b :d] [:a :a :a] [:a :a :b])
+;;  => #<Graph 80dc0041-0476-1196-9bc3-7831c1bbb832 (4 edges)>
 
 
 
@@ -250,16 +249,16 @@
    [s p o] {:- {[S P] {O (tuple S P O)}}}})
 
 (defn- del-triple [g [S P O]]
-  (let [tups (disj (triples g) (tuple S P O))]
+  (let [tups (disj (edges g) (tuple S P O))]
     (or (@db tups)
-      (let [k (keys (indices g))
+      (let [k (keys (indexes g))
             p (map (deindex-triple [S P O]) k)
-            o (map (indices g) k)
+            o (map (indexes g) k)
             n (zipmap k (map diff/patch-unchecked o p))]
         (->Graph (node) n tups)))))
 
-(defn del-triples
-  "Efficiently delete and deindex 0 or more triples from an existing graph,
+(defn del-edges
+  "Efficiently delete and deindex 0 or more edges from an existing graph,
   returning a new graph as the result."
   [g & more]
   (reduce del-triple g more))
@@ -269,15 +268,15 @@
 (defn- merge-index [g1 g2]
   (let [g1  (graph g1)
         g2  (graph g2)
-        k   (keys (indices g1))
-        ix1 (map (indices g1) k)
-        ix2 (map (indices g2) k)
+        k   (keys (indexes g1))
+        ix1 (map (indexes g1) k)
+        ix2 (map (indexes g2) k)
         ix  (map diff/merge* ix1 ix2)]
     (zipmap k ix)))
 
 
 (defn- merge-graph [g1 g2]
-  (let [tups (clojure.set/union (triples g1) (triples g2))]
+  (let [tups (clojure.set/union (edges g1) (edges g2))]
     (or (@db tups)
       (->Graph (node) (merge-index (graph g1) (graph g2)) tups))))
 
@@ -300,11 +299,11 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defn intern-graph [g]
-  (let [x (@db (triples g))]
+  (let [x (@db (edges g))]
     (if x
       (id x)
       (util/returning (id g)
-        (swap! db #(into % [[(id g) g] [(triples g) g]]))))))
+        (swap! db #(into % [[(id g) g] [(edges g) g]]))))))
   
 (def ^{:dynamic true}    *context* (graph (intern-graph (graph nil))))
 
@@ -320,10 +319,10 @@
 (extend-type java.util.UUID GraphContainer
              (id [this]
                this)
-             (indices [this]
-               (indices (graph this)))
-             (triples [this]
-               (triples (graph this))))
+             (indexes [this]
+               (indexes (graph this)))
+             (edges [this]
+               (edges (graph this))))
 
 
 (defmacro with-context [designator & body]
@@ -339,31 +338,31 @@
 ;;   (with-context #{[:dog :isa :animal]}
 ;;     (select +NULL+ [nil :isa nil])))
 ;;
-;;  => #<Graph 69b76850-05c5-1196-9bc3-7831c1bbb832 (2 triples)>
+;;  => #<Graph 69b76850-05c5-1196-9bc3-7831c1bbb832 (2 edges)>
 
 ;; (with-context #{[:fido :isa :dog]}
 ;;   (with-context #{[:dog :isa :animal]}
 ;;     (with-null-context
 ;;       (select +NULL+ [nil :isa nil]))))
 ;;
-;;  => #<Graph 00000000-0000-0000-0000-000000000000 (0 triples)>
+;;  => #<Graph 00000000-0000-0000-0000-000000000000 (0 edges)>
 
 
 
 ;; (assert (= (graph *context*) (graph nil) (graph 0)))
 
 ;; (graph #{[1 2 3]})
-;;  => #<Graph 2d1b48b0-723d-1195-8101-7831c1bbb832 (1 triples)>
-;;  => #<Graph 270f47a0-723d-1195-8101-7831c1bbb832 (1 triples)>
-;;  => #<Graph 257a01a0-723d-1195-8101-7831c1bbb832 (1 triples)>
+;;  => #<Graph 2d1b48b0-723d-1195-8101-7831c1bbb832 (1 edges)>
+;;  => #<Graph 270f47a0-723d-1195-8101-7831c1bbb832 (1 edges)>
+;;  => #<Graph 257a01a0-723d-1195-8101-7831c1bbb832 (1 edges)>
 
 ;; (intern-graph (graph #{[1 2 3]}))
 ;;  => #uuid "6d863860-723d-1195-8101-7831c1bbb832"
 
 ;; (graph #{[1 2 3]})
-;;  => #<Graph 6d863860-723d-1195-8101-7831c1bbb832 (1 triples)>
-;;  => #<Graph 6d863860-723d-1195-8101-7831c1bbb832 (1 triples)>
-;;  => #<Graph 6d863860-723d-1195-8101-7831c1bbb832 (1 triples)>
+;;  => #<Graph 6d863860-723d-1195-8101-7831c1bbb832 (1 edges)>
+;;  => #<Graph 6d863860-723d-1195-8101-7831c1bbb832 (1 edges)>
+;;  => #<Graph 6d863860-723d-1195-8101-7831c1bbb832 (1 edges)>
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -383,48 +382,48 @@
 (defmulti query supplied-constituents)
 
 (defmethod query [Graph false false false] [graph subj pred obj]
-  (triples graph))
+  (edges graph))
 
 
 (defmethod query [Graph true false false]  [graph subj pred obj]
-  (let [i0 ((indices graph) [s p o])
+  (let [i0 ((indexes graph) [s p o])
         i1 (get i0 subj)]
     (set (for [pred (keys i1)
                obj  (keys (get i1 pred))]
            (get-in i1 [pred obj])))))
 
 (defmethod query [Graph false true false]  [graph subj pred obj]
-  (let [i0 ((indices graph) [p o s])
+  (let [i0 ((indexes graph) [p o s])
         i1 (get i0 pred)]
     (set (for [obj  (keys i1)
                subj (keys (get i1 obj))]
            (get-in i1 [obj subj])))))
  
 (defmethod query [Graph false false true]  [graph subj pred obj]
-  (let [i0 ((indices graph) [o s p])
+  (let [i0 ((indexes graph) [o s p])
         i1 (get i0 obj)]
     (set (for [subj (keys i1)
                pred (keys (get i1 subj))]
            (get-in i1 [subj pred])))))
 
 (defmethod query [Graph true true false]  [graph subj pred obj]
-  (let [idx ((indices graph) [s p o])]
+  (let [idx ((indexes graph) [s p o])]
     (set (for [obj (keys (get-in idx [subj pred]))]
            (tuple subj pred obj)))))
  
 (defmethod query [Graph true false true]  [graph subj pred obj]
-  (let [idx ((indices graph) [o s p])]
+  (let [idx ((indexes graph) [o s p])]
     (set (for [pred (keys (get-in idx [obj subj]))]
            (tuple subj pred obj)))))
   
 (defmethod query [Graph false true true]  [graph subj pred obj]
-  (let [idx ((indices graph) [p o s])]
+  (let [idx ((indexes graph) [p o s])]
     (set (for [subj (keys (get-in idx [pred obj]))]
            (tuple subj pred obj)))))
  
 (defmethod query [Graph true true true]   [graph subj pred obj]
   (set (filter identity
-         (vector ((triples graph) [subj pred obj])))))
+         (vector ((edges graph) [subj pred obj])))))
 
 
 
@@ -459,41 +458,41 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; (select uuid/+null+ [1 2 3])
-;;   => #<Graph 00000000-0000-0000-0000-000000000000 (0 triples)>
+;;   => #<Graph 00000000-0000-0000-0000-000000000000 (0 edges)>
 
 ;; (select (graph #{[1 2 3] [4 5 6]}) [nil nil nil])
-;;   => #<Graph e9a62310-7238-1195-8101-7831c1bbb832 (2 triples)>    
+;;   => #<Graph e9a62310-7238-1195-8101-7831c1bbb832 (2 edges)>    
 
 ;; (with-context #{[1 2 3]}
 ;;   (select +NULL+ [1 2 nil]))
 ;;
-;;   => #<Graph 02a6e030-0536-1196-9bc3-7831c1bbb832 (1 triples)>
+;;   => #<Graph 02a6e030-0536-1196-9bc3-7831c1bbb832 (1 edges)>
 
 ;; (with-context #{[1 2 3]}
-;;   (triples (select +NULL+ [1 2 nil])))
+;;   (edges (select +NULL+ [1 2 nil])))
 ;;
 ;;   => #{[1 2 3]}
 
 ;; (with-context (graph #{[1 2 4]})
 ;;   (with-context #{[1 2 5]} 
-;;     (triples (select +NULL+ [1 2 nil]))))
+;;     (edges (select +NULL+ [1 2 nil]))))
 ;;
 ;; => #{[1 2 5] [1 2 4]}
 
 ;; (with-context (select (graph #{[1 2 3] [4 5 6]}) [nil nil nil])
-;;   (triples (select (graph nil) [nil nil nil])))
+;;   (edges (select (graph nil) [nil nil nil])))
 ;;   => #{[4 5 6] [1 2 3]}
 
 ;; (select (graph #{[1 2 3] [4 5 6]}) [nil nil nil])
-;;   => #<Graph 0322eb40-723c-1195-8101-7831c1bbb832 (2 triples)>
-;;   => #<Graph 0322eb40-723c-1195-8101-7831c1bbb832 (2 triples)>
-;;   => #<Graph 0322eb40-723c-1195-8101-7831c1bbb832 (2 triples)>
+;;   => #<Graph 0322eb40-723c-1195-8101-7831c1bbb832 (2 edges)>
+;;   => #<Graph 0322eb40-723c-1195-8101-7831c1bbb832 (2 edges)>
+;;   => #<Graph 0322eb40-723c-1195-8101-7831c1bbb832 (2 edges)>
 
 ;; (with-context (select (graph #{[1 2 3] [4 5 6]}) [nil nil nil])
 ;;   (select (graph nil) [nil nil nil]))
-;;   => #<Graph 0322eb40-723c-1195-8101-7831c1bbb832 (2 triples)>
-;;   => #<Graph 0322eb40-723c-1195-8101-7831c1bbb832 (2 triples)>
-;;   => #<Graph 0322eb40-723c-1195-8101-7831c1bbb832 (2 triples)>
+;;   => #<Graph 0322eb40-723c-1195-8101-7831c1bbb832 (2 edges)>
+;;   => #<Graph 0322eb40-723c-1195-8101-7831c1bbb832 (2 edges)>
+;;   => #<Graph 0322eb40-723c-1195-8101-7831c1bbb832 (2 edges)>
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -503,22 +502,22 @@
 (defn graph-union [g & more]
   (graph
     (reduce clojure.set/union
-      (map triples (conj more g)))))
+      (map edges (conj more g)))))
 
 
 (defn graph-intersection [g & more]
   (graph
     (reduce clojure.set/intersection
-      (map triples (conj more g)))))
+      (map edges (conj more g)))))
 
 
 (defn graph-difference [g & more]
   (graph
     (reduce clojure.set/difference
-      (map triples (conj more g)))))
+      (map edges (conj more g)))))
 
 
 (defn graph-equal? [g & more]
-  (every? #(= (triples g) %) (map triples more)))
+  (every? #(= (edges g) %) (map edges more)))
 
 
