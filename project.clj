62e65ca71665754de4067528f18c24023646fe29
Dan Lentz <danlentz@gmail.com>
2015-02-04 04:48:49 -0500

    depend: danlentz/clj-wbtree

--- project.clj
+++ project.clj
@@ -6,6 +6,7 @@
   :dependencies [[org.clojure/clojure    "1.6.0"]
                  [org.clojure/core.logic "0.8.8"]
                  [danlentz/clj-uuid      "0.0.7-SNAPSHOT"]
+                 [danlentz/clj-wbtree    "0.1.3-SNAPSHOT"]
                  [incanter               "1.5.5"]
                  [clj-tuple              "0.1.7"]
                  [print-foo              "1.0.1"]
